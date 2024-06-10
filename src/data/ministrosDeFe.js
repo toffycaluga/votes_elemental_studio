d94304a016871da716fb7594ee245d14d12ad9e5
Toffy Caluga <abraham.lillol@gmail.com>
2024-06-10 03:02:24 -0400

    feat/ministros de fe actualizados

---
+++ src/data/ministrosDeFe.js
@@ -0,0 +1,143 @@
+//? id sedes 
+
+// 'SEDE SOCIAL QUISCO NORTE':1,-
+// 'SEDE SOCIAL CORDILLERA':2,-
+// 'SEDE SOCIAL EL MIRADOR':3,-
+// 'SEDE SOCIAL VILLA MAIPUMAR':4,-
+// 'SEDE SOCIAL VILLA PADRE ALVEAR':5,-
+// 'SEDE SOCIAL LOS COPIHUES':6,-
+// 'SEDE SOCIAL UNION COMUNAL DE JUNTAS DE VECINOS':7,-
+// 'EX HOTEL ITALIA':8,-
+// 'SEDE SOCIAL AGUAS CLARAS':9,-
+// 'SEDE SOCIAL LOS OLIVOS':10,-
+// 'SEDE SOCIAL VILLA NUEVA':11,-
+// 'SEDE SOCIAL COMUNITARIA ISLA NEGRA':12,-
+// 'ESCUELA BÁSICA EL TOTORAL':13-
+
+
+const periodo = 2024;
+
+
+const ministrosDeFe = [
+    {
+        name: 'Estefanía Toledo Arcos',
+        functions: 'Ministro de Fe',
+        username: 'etoledo',
+        password: 'etoledo' + periodo.toString(),
+        periodo,
+        sede_id: 1,
+        mesa: 'M1'
+    },
+    {
+        name: 'Jennifer Rojas Osorio',
+        functions: 'Ministro de Fe',
+        username: 'jrojas',
+        password: 'jrojas' + periodo.toString(),
+        periodo,
+        sede_id: 4,
+        mesa: 'M1'
+    },
+    {
+        name: 'María Fernanda Ahumada Ramirez',
+        functions: 'Ministro de Fe',
+        username: 'mahumada',
+        password: 'mahumada' + periodo.toString(),
+        periodo,
+        sede_id: 3,
+        mesa: 'M1'
+    },
+    {
+        name: 'Fabian Ignacio Gonzalez Araya',
+        functions: 'Ministro de Fe',
+        username: 'fgonzalez',
+        password: 'fgonzalez' + periodo.toString(),
+        periodo,
+        sede_id: 13,
+        mesa: 'M1'
+    },
+    {
+        name: 'Leonel Moyano Bianchi',
+        functions: 'Ministro de Fe',
+        username: 'lmoyano',
+        password: 'lmoyano' + periodo.toString(),
+        periodo,
+        sede_id: 2,
+        mesa: 'M1'
+    },
+    {
+        name: 'Sandra Cardenas Marchant',
+        functions: 'Ministro de Fe',
+        username: 'scardenas',
+        password: 'scardenas' + periodo.toString(),
+        periodo,
+        sede_id: 5,
+        mesa: 'M1'
+    },
+    {
+        name: 'Thania Tapia González',
+        functions: 'Ministro de Fe',
+        username: 'ttapia',
+        password: 'ttapia' + periodo.toString(),
+        periodo,
+        sede_id: 12,
+        mesa: 'M1'
+    },
+    {
+        name: 'Melani Peña Romero',
+        functions: 'Ministro de Fe',
+        username: 'mpeña',
+        password: 'mpeña' + periodo.toString(),
+        periodo,
+        sede_id: 11,
+        mesa: 'M1'
+    },
+    {
+        name: 'Patricia Garces Romero',
+        functions: 'Ministro de Fe',
+        username: 'pgarces',
+        password: 'pgarces' + periodo.toString(),
+        periodo,
+        sede_id: 9,
+        mesa: 'M1'
+    },
+    {
+        name: 'José Martinez Santibañez',
+        functions: 'Ministro de Fe',
+        username: 'jmartinez',
+        password: 'jmartinez' + periodo.toString(),
+        periodo,
+        sede_id: 6,
+        mesa: 'M1'
+    },
+    {
+        name: 'Stephanie Toro Alvarez',
+        functions: 'Ministro de Fe',
+        username: 'storo',
+        password: 'storo' + periodo.toString(),
+        periodo,
+        sede_id: 7,
+        mesa: 'M1'
+    },
+    {
+        name: 'Jaqueline Vidal Ferri',
+        functions: 'Ministro de Fe',
+        username: 'jvidal',
+        password: 'jvidal' + periodo.toString(),
+        periodo,
+        sede_id: 8,
+        mesa: 'M1'
+    },
+    {
+        name: 'Daniela Espinoza Cerda',
+        functions: 'Ministro de Fe',
+        username: 'despinoza',
+        password: 'despinoza' + periodo.toString(),
+        periodo,
+        sede_id: 10,
+        mesa: 'M1'
+    },
+
+
+]
+
+console.log("ministros de fe : ", ministrosDeFe)
