2563d7c96534f636bf855cda2d6424dcb1ebd631
Toffy Caluga <abraham.lillol@gmail.com>
2024-06-10 02:36:53 -0400

    feat/digitadores actualizados

---
+++ src/data/digitadores.js
@@ -0,0 +1,142 @@
+//? id sedes 
+
+// 'SEDE SOCIAL QUISCO NORTE'= 1,-
+// 'SEDE SOCIAL CORDILLERA'= 2,-
+// 'SEDE SOCIAL EL MIRADOR'= 3,-
+// 'SEDE SOCIAL VILLA MAIPUMAR'= 4,-
+// 'SEDE SOCIAL VILLA PADRE ALVEAR'= 5,-
+// 'SEDE SOCIAL LOS COPIHUES'= 6,-
+// 'SEDE SOCIAL UNION COMUNAL DE JUNTAS DE VECINOS'= 7,-
+// 'EX HOTEL ITALIA'= 8,-
+// 'SEDE SOCIAL AGUAS CLARAS'= 9,-
+// 'SEDE SOCIAL LOS OLIVOS'= 10,-
+// 'SEDE SOCIAL VILLA NUEVA'= 11,-
+// 'SEDE SOCIAL COMUNITARIA ISLA NEGRA'= 12,-
+// 'ESCUELA BÁSICA EL TOTORAL'= 13-
+
+const periodo = 2024;
+
+
+export const digitadores = [
+    {
+        name: 'Aurora Bustamante Navarrete',
+        functions: 'Digitador',
+        username: 'abustamante',
+        password: 'abustamante' + periodo.toString(),
+        periodo,
+        sede_id: 1,
+        mesa: 'M1'
+    },
+    {
+        name: 'Daniela Rojas Cortés',
+        functions: 'Digitador',
+        username: 'drojas',
+        password: 'drojas' + periodo.toString(),
+        periodo,
+        sede_id: 4,
+        mesa: 'M1'
+    },
+    {
+        name: 'Rossana Buston Briones',
+        functions: 'Digitador',
+        username: 'rbuston',
+        password: 'rbuston' + periodo.toString(),
+        periodo,
+        sede_id: 3,
+        mesa: 'M1'
+    },
+    {
+        name: 'Minori Azañedo Figueroa',
+        functions: 'Digitador',
+        username: 'mazañedo',
+        password: 'mazañedo' + periodo.toString(),
+        periodo,
+        sede_id: 13,
+        mesa: 'M1'
+    },
+    {
+        name: 'Gonzalo Concha Neira',
+        functions: 'Digitador',
+        username: 'gconcha',
+        password: 'gconcha' + periodo.toString(),
+        periodo,
+        sede_id: 2,
+        mesa: 'M1'
+    },
+    {
+        name: 'Erika Toro Donoso',
+        functions: 'Digitador',
+        username: 'etoro',
+        password: 'etoro' + periodo.toString(),
+        periodo,
+        sede_id: 5,
+        mesa: 'M1'
+    },
+    {
+        name: 'Gilda González Cepeda',
+        functions: 'Digitador',
+        username: 'ggonzalez',
+        password: 'ggonzalez' + periodo.toString(),
+        periodo,
+        sede_id: 12,
+        mesa: 'M1'
+    },
+    {
+        name: 'Javier Castro Carrasco',
+        functions: 'Digitador',
+        username: 'jcastro',
+        password: 'jcastro' + periodo.toString(),
+        periodo,
+        sede_id: 11,
+        mesa: 'M1'
+    },
+    {
+        name: 'Amparo Carreño Bustos',
+        functions: 'Digitador',
+        username: 'acarreño',
+        password: 'acarreño' + periodo.toString(),
+        periodo,
+        sede_id: 9,
+        mesa: 'M1'
+    },
+    {
+        name: 'Camila Morales Figueroa',
+        functions: 'Digitador',
+        username: 'cmorales',
+        password: 'cmorales' + periodo.toString(),
+        periodo,
+        sede_id: 6,
+        mesa: 'M1'
+    },
+    {
+        name: 'Manuel Galdames Herrera',
+        functions: 'Digitador',
+        username: 'mgaldames',
+        password: 'mgaldames' + periodo.toString(),
+        periodo,
+        sede_id: 7,
+        mesa: 'M1'
+    },
+    {
+        name: 'Patricio Carreño Valenzuela',
+        functions: 'Digitador',
+        username: 'pcarreño',
+        password: 'pcarreño' + periodo.toString(),
+        periodo,
+        sede_id: 8,
+        mesa: 'M1'
+    },
+    {
+        name: 'Leslie Turra Maldonado',
+        functions: 'Digitador',
+        username: 'lturra',
+        password: 'lturra' + periodo.toString(),
+        periodo,
+        sede_id: 10,
+        mesa: 'M1'
+    },
+
+];
+
+
+console.log("digitadores : ", digitadores)
