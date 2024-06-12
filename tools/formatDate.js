f65444620f088c801ec2b0af484515d23f4ac01d
Toffy Caluga <abraham.lillol@gmail.com>
2024-06-11 20:49:18 -0400

    feat/validacion de votos en vivo

--- tools/formatDate.js
+++ tools/formatDate.js
@@ -1,7 +1,7 @@
 import moment from "moment";
 export default function formatDate(date) {
 
-    const dateFormat = moment(date).format('L');
+    const dateFormat = moment(date).format('YYYY-MM-DD');
     return dateFormat;
 
 
